f358f0769b13dec2524f7a426fbae4e21b7971ff
Mauricio Reyna <mauriciojreyna@gmail.com>
2019-02-06 09:29:18 -0500

    FINISH

---
+++ GuesserLab/Guesser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuesserLab
+{
+    class Guesser
+    {
+        public string Name { get; set; }
+        public int CurrentCount { get; set; }
+        public int MaxRandomNum { get; set; }   
+        public int TimesRan { get; set; }
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public double Average { get; set; }
+        public List<int> Count = new List<int> { };
+
+        public void PrintStats()
+        {
+            this.Average = (Count.Sum() / TimesRan) * 1.0;
+            Console.WriteLine($"{Name} ran {TimesRan} times.");
+            Console.WriteLine($"Average: {this.Average}");
+            Console.WriteLine($"Min: {this.Min}");
+            Console.WriteLine($"Max: {this.Max}");
+            Console.WriteLine();
+        }
+        public void Stats()
+        {
+            if (CurrentCount < Min)
+            {
+                Min = CurrentCount;
+            }
+            if (CurrentCount > Max)
+            {
+                Max = CurrentCount;
+            }
+            EachCounts.Add(CurrentCount);
+            this.CurrentCount = 0;
+        }
+    }
+}
---
+++ GuesserLab/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuesserLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int computerNumer = GetNumber(0, 100);
+
+
+            bool stillGuessing = true;
+            string message = "Give me a number between 0 and 100";
+            int count = 1;
+
+            while (stillGuessing)
+            {
+
+                int userNumber = GetUserNumber(message, 0, 100);
+                int difference = DoSubtraction(userNumber, computerNumer);
+
+                if (difference == 0)
+                {
+                    message = "Congratulations! Yay! You did it!";
+                }
+                else
+                {
+                    count++;
+                    if (difference > 0)
+                    {
+
+                        if (difference >= 10)
+                        {
+                            message = "Up on your HIGH horse ain't ya?";
+                        }
+                        else
+                        {
+                            message = "Too high.";
+                        }
+                    }
+                    else
+                    {
+
+                        if (difference <= -10)
+                        {
+                            message = "Shawty got LOW-LOW-LOW-LOW-LOW-LOW-LOW-LOW";
+                        }
+                        else
+                        {
+                            message = "Too low";
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(message);
+        }
+
+        public static int DoSubtraction(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public static int GetNumber(int min, int max)
+        {
+            Random r = new Random();
+            return r.Next(min, max);
+        }
+
+        public static int GetUserNumber(string message, int min, int max)
+        {
+            int userNumber = min - 1;
+            Console.WriteLine(message);
+
+            if (userNumber < min || userNumber > max)
+            {
+                while (!int.TryParse(Console.ReadLine(), out userNumber)) ;
+            }
+
+            return userNumber;
+
+
+        }
+    }
+}
+
